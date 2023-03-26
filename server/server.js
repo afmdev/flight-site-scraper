d04e12f4fa6c2895313dde1ed1b9a3810f561ce1
Alejandro FM <102042032+alejanfm@users.noreply.github.com>
2023-03-26 16:15:25 +0200

    Adding cloudinary and passport

--- server/server.js
+++ server/server.js
@@ -1,18 +1,64 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import * as dotenv from 'dotenv'
 
+import { cloudinaryConfig } from './config/cloudinaryConfig.js'
 
-const app = express();
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-	console.log("Alejandro: Your server is running on " + port + "port");
-});
+import passportConfig from './config/passport.js'
+import passport from 'passport'
 
+const app = express()
+dotenv.config()
+const port = process.env.PORT || 5000
 
-app.use(express.json());
-app.use(
-	express.urlencoded({
-		extended: true,
+const startServer = () => {
+	app.listen(port, () => {
+		console.log("|-->>>>>>> The backend server is running on port " + port + " <<<<<<<--|")
 	})
-);
-app.use(cors());
+}
+
+const loadRoutes = () => {
+	app.use("/api/categories", categoriesRoute)
+	app.use("/api/genders", gendersRoute)
+	app.use("/api/items", itemsRoute)
+	app.use("/api/users", usersRoute)
+	app.use("/api/carts", cartsRoute)
+	app.use("/api/orders", ordersRoute)
+}
+
+const addMiddleware = () => {
+	app.use(express.json())
+	app.use(
+		express.urlencoded({
+			extended: true,
+		})
+	)
+	const corsOptions = {
+		origin: '*',
+		credentials: true,
+		//access-control-allow-credentials:true
+		optionSuccessStatus: 200,
+	}
+	app.use(cors(corsOptions))
+	cloudinaryConfig()
+
+	app.use(passport.initialize())
+	passportConfig(passport)
+}
+
+const mongoDbConection = async () => {
+	try {
+		await mongoose.connect(process.env.DB)
+		console.log("|-->>>>>> The connection to MongoDB has been successful <<<<<--| ")
+	} catch (error) {
+		console.log("ERROR: fail connecting to Mongo DB", error)
+	}
+}
+
+(async () => {
+	await mongoDbConection()
+	// addMiddleware()
+	// loadRoutes()
+	startServer()
+})()
